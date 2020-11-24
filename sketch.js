27e03160c968d9c8e58f98f33218adc0527abc8f
Nicolas Jimenez <55262023+fnj-nicolas@users.noreply.github.com>
2020-11-24 11:27:16 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var canvas;
-let estado = 'pantalla12';
+let estado = 'pantalla6';
 let fondos;
 let timer = 0;
 
@@ -7,12 +7,16 @@ let time = 0;
 let wait = 1000;
 let timer1 = 0;
 
+
 function setup() {
   createCanvas(800, 600);
   
   btnIniciar = new Clickable();
-  btnIniciar.locate(width/2, height/2);
-  btnIniciar.text = 'INICIAR';
+  btnIniciar.locate(width/2-135, height/2+60);
+  btnIniciar.color = "#66000000"; 
+  btnIniciar.resize(275, 60);   
+  btnIniciar.text = ' ';
+  btnIniciar.strokeWeight = 0;
   btnIniciar.onPress = function(){
     estado = 'pantalla1';
     print('pantalla1');
@@ -22,6 +26,23 @@ function setup() {
   btnVecino.locate(width/2-300,height/2);
   btnVecino.onPress = function(){
     estado = 'pantalla3';
+  }
+
+  btnCortina = new Clickable();
+  btnCortina.locate(10,height/2+100)
+  btnCortina.onPress = function(){
+    estado = 'perderHermana';
+  }
+
+  btnBilletera = new Clickable();
+  btnBilletera.locate(width/2+300,height/2+200);
+  btnBilletera.onPress = function(){
+    estado = "perder1"
+  }
+
+  btnVolverJugar = new Clickable();
+  btnVolverJugar.onPress = function(){
+    estado = "inicio";
   }
 
   timerHermana = new Clickable();
@@ -63,12 +84,15 @@ function setup() {
 
   btnGuitarra3 = new Clickable();
   btnGuitarra3.locate(90,height/2+150);
+  btnGuitarra3.text = 'holas gente';
   btnGuitarra3.onPress = function(){
     estado = "pantalla15"
   }
 }
 
 function preload(){
+  inicio =  loadImage ('fondo/inicio.png');
+
   concurso01 = loadImage('fondo/concurso01.png');
   pensando02 = loadImage('fondo/pensando02.png');
   vecino03 = loadImage('fondo/vecino03.png');
@@ -94,11 +118,18 @@ function preload(){
   conQueTocar = loadImage('fondo/conQueTocar.png');
 
   tocarSinAmpli = loadImage('fondo/tocarSinAmpli.png');
+
+  final = loadImage('fondo/fin.png');
+  marca_final = loadImage('fondo/marca_final.png');
+  creditos_final = loadImage('fondo/creditos_final.png');
+
+  perderMama1 = loadImage('fondo/perderMama1.png');
+  perderHermana2 = loadImage('fondo/perderHermana2.png');
 }
 
 function draw() {
-  background(255,0,0);
   if (estado== "inicio"){
+    image(inicio,0,0,800,600);
     btnIniciar.draw();
   }
   if(estado=='pantalla1'){
@@ -124,6 +155,13 @@ function draw() {
   if (estado == 'pantalla2'){
     image(billeteopasto,0,0,800,600); 
     btnVecino.draw();
+    btnBilletera.draw();
+  }
+  if(estado == "perder1"){
+    image(perderMama1,0,0,800,600);
+    
+    btnVolverJugar.locate(width/2,height/2);
+    btnVolverJugar.draw();
   }
   if (estado == 'pantalla3'){
     if (millis() - time >= wait) {
@@ -144,24 +182,37 @@ function draw() {
     else if (timer >= 5 && timer <= 7){
       image(ani_hermana1,0,0,800,600);
     }
-    else if (timer >= 7 && timer <= 9){
+    else if (timer >= 7 && timer <= 10){
       image(ani_hermana2,0,0,800,600);
     }
-    else if (timer >= 9){
+    else if (timer >= 10){
       estado = "pantalla4";
       timer = 0;
     }
   }
   if( estado == "pantalla4"){
+    let tiempo = 9;
     if (millis() - time >= wait) {
       timer++;
       print(timer);//if it is, do something
       time = millis();//also update the stored time
     }
+
+    tiempo = tiempo - timer;
+    if(tiempo <= 0){
+      estado = "perderHermana";
+    }
     image(pantalla04,0,0,800,600); 
     timerHermana.draw();
-    timerHermana.text = str(timer);
+    btnCortina.draw();
+    timerHermana.text = str(tiempo);
     btnArmario.draw();
+    btnArmario.text = "btnArmario";
+  }
+  if(estado =="perderHermana"){
+    image(perderHermana2,0,0,800,600);
+    btnVolverJugar.locate(width/2,height/2+100);
+    btnVolverJugar.draw();
   }
   if(estado == "pantalla5"){
     if (millis() - time >= wait) {
@@ -180,14 +231,21 @@ function draw() {
     }
   }
   if(estado == "pantalla6"){
+    let tiempo = 9;
     if (millis() - time >= wait) {
       timer++;
       print(timer);//if it is, do something
       time = millis();//also update the stored time
     }
+
+    tiempo = tiempo - timer;
     image(altillo,0,0,800,600);
+
+    if(tiempo <= 0){
+      estado = "perderHermana";
+    }
     timerAltillo.draw();
-    timerAltillo.text =timer;
+    timerAltillo.text = tiempo;
     btnGuitarra.draw();
   }
   if(estado == "pantalla7"){
@@ -213,7 +271,8 @@ function draw() {
       print(timer);//if it is, do something
       time = millis();//also update the stored time
     }
-    if(timer >= 2){
+    if(timer >= 3){
+      timer = 0;
       estado = "pantalla10";
     }
   }
@@ -221,7 +280,7 @@ function draw() {
     image(elegirGuitarra_Fiesta,0,0,800,600);
     btnGuitarra2.draw();
   }
-  if(estado == "pantalla11"){
+  if(estado == "pantalla11"){     ////////////////////
     image(juanGuitarraCampo,0,0,800,600);
     if (millis() - time >= wait) {
       timer++;
@@ -253,7 +312,23 @@ function draw() {
     btnGuitarra3.draw();
   }
   if(estado == "pantalla15"){
-    image(tocarSinAmpli,0,0,800,600);
+    if (millis() - time >= wait) {
+      timer++;
+      print(timer);//if it is, do something
+      time = millis();//also update the stored time
+    }
+    if(timer>=0 && timer <= 10){
+      image(tocarSinAmpli,0,0,800,600);
+    }
+    if(timer >= 10 && timer <= 12){
+      image(final,0,0,800,600);
+    }
+    else if(timer >= 12 && timer <= 15){
+      image(marca_final,0,0,800,600);
+    }
+    else if(timer >= 15 && timer <= 20){
+      image(creditos_final,0,0,800,600);
+    }
   }
 }
 
